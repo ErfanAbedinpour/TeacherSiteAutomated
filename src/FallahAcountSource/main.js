0cf90fcd641c443b5738eb1a9ce1198161742127
ErfanAbedinpour <erfan.wtf44@gmail.com>
2023-12-18 22:54:30 +0330

    Added Get Answer In main

--- src/FallahAcountSource/main.js
+++ src/FallahAcountSource/main.js
@@ -1,17 +1,12 @@
 const { JSDOM } = require("jsdom");
 const superagent = require("superagent");
-const { getAnswer } = require("./GetQuizAnswer");
-class User {
-  //Constructor for Get User Coockie
-  constructor() {
-    this.Get_Coockie = async function GetCoockie() {
-      var Response = await superagent.post(
-        "http://baazmooon.ir/signup_sub.php"
-      );
-      return await Response.headers["set-cookie"][0].split(";")[0];
-    };
-  }
+// const Answer = require("./GetQuizAnswer");
 
+class User {
+  static async Get_Coockie() {
+    var Response = await superagent.post("http://baazmooon.ir/signup_sub.php");
+    return await Response.headers["set-cookie"][0].split(";")[0];
+  }
   //Login on Fallah Account
   //isTeacher = is user is teacher change false if User is Student must true
   static async Login(email, password, isTeacher = false, callback) {
@@ -82,13 +77,45 @@ class User {
     });
   }
 
-  static async GetAnswer(course, pdmn) {
-    await getAnswer(course, pdmn, (datas) => {
-      console.log(datas);
+  static Answer(course, pdmn, callback) {
+    this.Login("fallah@aram.khd", "12345608", true, async (name, coockie) => {
+      var Response = await superagent
+        .post("http://baazmooon.ir/addquestion.php")
+        .set(
+          "Accept",
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+        )
+        .set(
+          "Accept-Language",
+          "fa-IR,fa;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5"
+        )
+        .set("Cache-Control", "max-age=0")
+        .set("Connection", "keep-alive")
+        .set("Cookie", coockie)
+        .set("Origin", "http://baazmooon.ir")
+        .set("Referer", "http://baazmooon.ir/azmoon.php")
+        .set("Upgrade-Insecure-Requests", "1")
+        .type("form")
+        .send({
+          cat: String(course),
+          pdmn: String(pdmn),
+        });
+
+      const dom = new JSDOM(Response.text);
+      const tags = dom.window.document.querySelectorAll("td");
+      const result = [];
+      let answer = 3;
+      for (let id = 0; id < tags.length; id += 6) {
+        var CreateObject = {};
+        CreateObject["id"] = tags[id].textContent;
+        CreateObject["answer"] = +tags[answer].textContent;
+        result.push({ ...CreateObject });
+        answer += 6;
+      }
+      callback(result);
     });
   }
 }
 
-console.log(User.GetAnswer(1, 1));
-
-module.exports = User;
+// User.GetAnswer(1, 1);
+module.exports = { Site: User };
