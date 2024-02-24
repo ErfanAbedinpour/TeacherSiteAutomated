950679a70db0c80b8ec21c9224292d8488044186
Erfna <erfan.wtf44@gmail.com>
2024-02-24 12:31:47 +0000

    this is Complete For me

--- src/bot/start.js
+++ src/bot/start.js
@@ -1,6 +1,6 @@
 const Telegram = require("node-telegram-bot-api");
 require('dotenv').config
-const bot = new Telegram(process.env['BotToken'], {
+const bot = new Telegram("6903234677:AAGMzTgveht0K0bkGXPrs8ToiKJUOmoXXwc", {
   polling: true,
 });
 
