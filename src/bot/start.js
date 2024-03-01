7e88c329f5db994f40847de94ba04c221147a225
Erfna <erfan.wtf44@gmail.com>
2024-03-01 08:07:39 +0000

    Finish

--- src/bot/start.js
+++ src/bot/start.js
@@ -1,6 +1,6 @@
 const Telegram = require("node-telegram-bot-api");
 require('dotenv').config
-const bot = new Telegram("6903234677:AAGMzTgveht0K0bkGXPrs8ToiKJUOmoXXwc", {
+const bot = new Telegram("", {
   polling: true,
 });
 
